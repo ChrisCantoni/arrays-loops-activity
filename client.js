4f47119bc82c3266a0c2f46c31ede1c105e93d3b
Chris Cantoni <chris.cantoni@gmail.com>
2023-08-23 11:32:11 -0500

    Group activity solutions mostly complete.

--- client.js
+++ client.js
@@ -7,6 +7,12 @@
  *    hobbies
  */
 
+let hobbies = ['guitar', 'video games', 'netflix', 'biking'];
+
+for (let fun of hobbies) {
+    console.log(fun);
+}
+console.log(hobbies.length);
 
 // Example output
 // 1. swimming
@@ -25,6 +31,16 @@
  * 4. Output the array and number of times teal was found
  */
 
+let colors = ['blue', 'teal', 'red', 'green', 'teal'];
+let tealCount = 0;
+
+for (let color of colors) {
+    if (color == 'teal') {
+        tealCount++;
+    }
+}
+console.log('Times teal was found:', tealCount);
+
 
 // Example output
 // green, red, teal, orange, teal
@@ -41,6 +57,20 @@
  * 4. Output the original array, odd number array and even number array
  */
 
+let numbers = [2, 4, 6, 8, 27, 33, 45, 19];
+let oddNumbers = [];
+let evenNumbers = [];
+
+for (let n of numbers) {
+    if (n % 2 == 0) {
+        evenNumbers.push(n);
+    } else {
+        oddNumbers.push(n);
+    }
+}
+console.log(numbers);
+console.log(oddNumbers);
+console.log(evenNumbers);
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
@@ -57,6 +87,20 @@
  * 4. Output both arrays
  */
 
+let booleanValues = [true, true, false, true, false, true];
+let toggled = [];
+
+for (let value of booleanValues) {
+    if (value) {
+        value = false;
+        toggled.push(value);
+    } else {
+        value = true;
+        toggled.push(value);
+    }
+}
+console.log(booleanValues);
+console.log(toggled);
 
 // Example output
 // true, false, true, true
@@ -73,6 +117,16 @@
  * 3. Output the array
  */
 
+let zeroNumbers = [3, 5, 0, 2, 0, 0]
+
+for (i = (zeroNumbers.length - 1); i>=0; i--) {
+    if (zeroNumbers[i] === 0) {
+        zeroNumbers.pop();
+    } else {
+        break;
+    }
+}
+console.log(zeroNumbers);
 
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
@@ -91,6 +145,13 @@
  * 3. Output the array and its greatest position distance
  */
 
+let largeNumbers = [2, 4, 6, 6, 3, 1, 2]; //answer should be 6
+
+for (let p = 0; p > largeNumbers.length; p++) {
+    if (largeNumbers[p] = largeNumbers[(p+1)]) {
+        
+    }
+}
 
 // Example output
 // 5, 1, 8, 2, 9, 1, 4, 5, 0
